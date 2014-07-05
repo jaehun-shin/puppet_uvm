c6670e59a10c26c1cd60886828ebed78777ffd0a
Shin Jaehun <gunsmoke.shin@gmail.com>
2014-07-05 17:38:42 +0900

    Added a file resource to testserver.

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,12 @@ node 'losttemple.linuxmaster.com' {
 #	include let_it_go
 }
 
+node 'uvm01.linuxmaster.com' {
+	file {'/tmp/testserver':
+		content => "This is a test server.\n",
+	}
+}
+
 node /^uvm[0-9]+\.linuxmaster\.com$/ {
 #	include sshd
 #	include localmail
